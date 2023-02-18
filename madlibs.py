a8b30b999e114fe0e4da56e0193efde662543a52
Amrit Tyagi <87172993+TyagiAmrit271@users.noreply.github.com>
2023-02-18 13:11:55 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+# madlibs by using the concept of string concatination
+# The format used here is f string format which is used by simply using the letter 'f' before the print statement
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Programming language is so {adj}! it makes me so excited because\
+ {verb1}. drink water and {verb2} because you are {famous_person}!"
+
+print(madlib)
