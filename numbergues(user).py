c2df34e79f5408aa510527864d8b64862521bd40
Amrit Tyagi <87172993+TyagiAmrit271@users.noreply.github.com>
2023-02-19 18:03:36 +0530

    Update numbergues(user).py

--- numbergues(user).py
+++ numbergues(user).py
@@ -1,15 +1,15 @@
-import random
+import random #to access random library
 
 def computer_guess(x):
     low = 1
     high = x
     feedback = ''
     while feedback!= 'c':
-        if low!=high:
-            guess = random.randint(low,high)
+        if low!=high: #if statement to avoid error caused by randit
+            guess = random.randint(low,high) #to generate a random number between lower limit which is 1 and higher limit which is defined by user
         else:
             guess = low
-        feedback = input(f'is {guess} high(H),low(L), or Correct(C)??').lower()
+        feedback = input(f'is {guess} high(H),low(L), or Correct(C)??').lower() #.lower to convert feedback in lower case 
         if feedback == 'h':
             high = guess - 1
         elif feedback == 'l':
